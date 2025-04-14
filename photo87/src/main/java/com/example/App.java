9943754821605196560e5a396b0f6346b8302f87
jihyungkim4 <jihyung.kim4@gmail.com>
2025-04-14 13:15:06 -0400

    fixed error label in admin screen, set icon for app, and login resizes properly

--- photo87/src/main/java/com/example/App.java
+++ photo87/src/main/java/com/example/App.java
@@ -32,6 +32,8 @@ public class App extends Application {
         scene = new Scene(loadFXML("login"), 640, 480);
         stage.setScene(scene);
 
+        stage.getIcons().add(new javafx.scene.image.Image(getClass().getResourceAsStream("icon.png")));
+
         stage.setOnCloseRequest(event -> {
             Alert alert = new Alert(AlertType.CONFIRMATION);
             alert.setTitle("Exit Confirmation");
@@ -161,6 +163,9 @@ public class App extends Application {
         user = null;
         currentAlbum = null;
         App.setRoot("login");
+        stage.setWidth(640);
+        stage.setHeight(480);
+        stage.centerOnScreen();
     }
 
     public static void main(String[] args) {
